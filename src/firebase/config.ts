fa26e98dfd53009a2231947f4ede627c5e422471
Nanang Nurmansah <nanangnurmansah5@gmail.com>
2025-09-25 06:19:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9770915359-588c5",
+  "appId": "1:312456334974:web:99452a88485bc0698ad22f",
+  "apiKey": "AIzaSyDYCo9d5uxtTYNKXj8njYKmh91aV9x-bcI",
+  "authDomain": "studio-9770915359-588c5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "312456334974"
+};
